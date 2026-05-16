06306a279af3157a6692b5490a435e7885facdc0
jimvankuik <jim.vankuik@gmail.com>
2026-05-16 23:14:26 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v16-zoom-controls",
-  worldWidth: 3100,
+  version: "v19-no-regression-ui",
+  worldWidth: 3300,
   gravityY: 1120,
   playerSpeed: 260,
   jumpVelocity: -560
