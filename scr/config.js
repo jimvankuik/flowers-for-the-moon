23bf637de0516d860ec585ce2717f951f599cfeb
jimvankuik <jim.vankuik@gmail.com>
2026-05-16 19:46:28 +0200

    Add files via upload

---
+++ scr/config.js
@@ -0,0 +1,9 @@
+const GAME_SETTINGS = {
+  width: 960,
+  height: 540,
+  gravity: 950,
+  playerSpeed: 245,
+  jumpSpeed: 470,
+  flowerGoal: 5,
+  easyMode: true
+};
--- scr/main.js
+++ scr/main.js
@@ -1,1 +1,26 @@
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: '#0d1430',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: GAME_SETTINGS.gravity },
+      debug: false
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  input: {
+    activePointers: 4
+  },
+  scene: [BootScene, LevelScene]
+};
 
+window.addEventListener('load', () => {
+  new Phaser.Game(config);
+});
