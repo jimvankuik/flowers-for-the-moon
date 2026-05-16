0e3adc0e3622e33ad979c4c7d893665d05606a57
jimvankuik <jim.vankuik@gmail.com>
2026-05-16 21:17:00 +0200

    Add files via upload

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,20 +1,5 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.Level1 = {
-  platforms: [
-    { x: 0, y: 500, w: 560, h: 40 },
-    { x: 720, y: 430, w: 300, h: 34 },
-    { x: 1120, y: 365, w: 300, h: 34 },
-    { x: 1520, y: 420, w: 330, h: 34 },
-    { x: 2050, y: 500, w: 750, h: 40 }
-  ],
-  flowers: [
-    { x: 245, y: 450 },
-    { x: 835, y: 380 },
-    { x: 1265, y: 315 },
-    { x: 1675, y: 370 },
-    { x: 2245, y: 450 }
-  ],
-  moonBoy: { x: 2530, y: 155 },
-  finishZone: { x: 2485, y: 470, w: 270, h: 150 }
+  note: "Responsive level layout is calculated in LevelScene.js"
 };
