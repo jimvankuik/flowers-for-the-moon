425e99a0951b0bca9a554eb0a680d87259730134
jimvankuik <jim.vankuik@gmail.com>
2026-05-17 01:08:19 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v20-controls-camera",
+  version: "v21-movement-polish",
   worldWidth: 3300,
-  gravityY: 1120,
+  gravityY: 1220,
   playerSpeed: 260,
-  jumpVelocity: -560
+  jumpVelocity: -590,
+  acceleration: 1450,
+  deceleration: 1750
 };
