e699d59ce769238a981cbd7a4da5b068c68f4c15
jimvankuik <jim.vankuik@gmail.com>
2026-05-17 11:59:00 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,12 +1,2 @@
 window.FTTM = window.FTTM || {};
-
-window.FTTM.GameSettings = {
-  version: "v24-camera-variable-jump",
-  worldWidth: 3300,
-  gravityY: 1180,
-  playerSpeed: 260,
-  jumpVelocity: -575,
-  jumpCutVelocity: -260,
-  acceleration: 1400,
-  deceleration: 1700
-};
+window.FTTM.GameSettings = { version:'v25-camera-ui', worldWidth:3300, gravityY:1180, playerSpeed:260, jumpVelocity:-575, jumpCutVelocity:-260, acceleration:1400, deceleration:1700 };
