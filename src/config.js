10e3e68631638a6d75cde230b8718cc95f0b5ca6
jimvankuik <jim.vankuik@gmail.com>
2026-05-17 14:24:48 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-window.FTTM = window.FTTM || {};
-window.FTTM.GameSettings = { version:'v27-fullscreen-camera-fix', worldWidth:3300, gravityY:1180, playerSpeed:260, jumpVelocity:-575, jumpCutVelocity:-260, acceleration:1400, deceleration:1700 };
+window.FTTM = window.FTTM || {  buildMarker: "v28-force-upload-config"
+};
+window.FTTM.GameSettings = { version:'v28-force-upload', worldWidth:3300, gravityY:1180, playerSpeed:260, jumpVelocity:-575, jumpCutVelocity:-260, acceleration:1400, deceleration:1700   buildMarker: "v28-force-upload-config"
+};
