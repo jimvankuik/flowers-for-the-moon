0ac91b6a455c81b26b04151998399b0feea0b543
jimvankuik <jim.vankuik@gmail.com>
2026-05-18 23:20:40 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,33 +1,35 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v40-platform-extend",
-  worldWidth: 4000,
+  version: "v41-platform-extend",
+  worldWidth: 4400,
   gravityY: 1680,
   playerSpeed: 335,
   jumpVelocity: -690,
   jumpCutVelocity: -310,
   acceleration: 2400,
   deceleration: 2850,
-  buildMarker: "v40-platform-extend-config"
+  buildMarker: "v41-end-platform-moon-fix-config"
 };
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
 
-// build-marker: v40-platform-extend-config
+// build-marker: v41-platform-extend-config
+
+// build-marker: v41-end-platform-moon-fix-config
