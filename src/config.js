584330fcc8b894b67fde551516a233ef5833b2b0
jimvankuik <jim.vankuik@gmail.com>
2026-05-18 20:32:57 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,29 +1,31 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v37-moon-visible-end",
-  worldWidth: 4300,
+  version: "v38-moon-finish-fix",
+  worldWidth: 4000,
   gravityY: 1180,
   playerSpeed: 260,
   jumpVelocity: -575,
   jumpCutVelocity: -260,
   acceleration: 1400,
   deceleration: 1700,
-  buildMarker: "v37-moon-visible-end-config"
+  buildMarker: "v38-moon-finish-fix-config"
 };
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
 
-// build-marker: v37-moon-visible-end-config
+// build-marker: v38-moon-finish-fix-config
+
+// build-marker: v38-moon-finish-fix-config
