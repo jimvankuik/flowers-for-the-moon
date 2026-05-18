39bc612367bb76b01366a98548bf9d37cb22f44d
jimvankuik <jim.vankuik@gmail.com>
2026-05-18 20:24:45 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,27 +1,29 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v36-moon-landscape-zoom",
-  worldWidth: 3800,
+  version: "v37-moon-visible-end",
+  worldWidth: 4300,
   gravityY: 1180,
   playerSpeed: 260,
   jumpVelocity: -575,
   jumpCutVelocity: -260,
   acceleration: 1400,
   deceleration: 1700,
-  buildMarker: "v36-moon-landscape-zoom-config"
+  buildMarker: "v37-moon-visible-end-config"
 };
 
-// build-marker: v36-moon-landscape-zoom-config
+// build-marker: v37-moon-visible-end-config
 
-// build-marker: v36-moon-landscape-zoom-config
+// build-marker: v37-moon-visible-end-config
 
-// build-marker: v36-moon-landscape-zoom-config
+// build-marker: v37-moon-visible-end-config
 
-// build-marker: v36-moon-landscape-zoom-config
+// build-marker: v37-moon-visible-end-config
 
-// build-marker: v36-moon-landscape-zoom-config
+// build-marker: v37-moon-visible-end-config
 
-// build-marker: v36-moon-landscape-zoom-config
+// build-marker: v37-moon-visible-end-config
 
-// build-marker: v36-moon-landscape-zoom-config
+// build-marker: v37-moon-visible-end-config
+
+// build-marker: v37-moon-visible-end-config
