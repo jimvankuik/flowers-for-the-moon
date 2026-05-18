f7b0fdbb9436f30743bafe4d946d6fbd189a93ef
jimvankuik <jim.vankuik@gmail.com>
2026-05-18 20:13:00 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,25 +1,27 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v35-landscape-camera-fix",
-  worldWidth: 3300,
+  version: "v36-moon-landscape-zoom",
+  worldWidth: 3800,
   gravityY: 1180,
   playerSpeed: 260,
   jumpVelocity: -575,
   jumpCutVelocity: -260,
   acceleration: 1400,
   deceleration: 1700,
-  buildMarker: "v35-landscape-camera-fix-config"
+  buildMarker: "v36-moon-landscape-zoom-config"
 };
 
-// build-marker: v35-landscape-camera-fix-config
+// build-marker: v36-moon-landscape-zoom-config
 
-// build-marker: v35-landscape-camera-fix-config
+// build-marker: v36-moon-landscape-zoom-config
 
-// build-marker: v35-landscape-camera-fix-config
+// build-marker: v36-moon-landscape-zoom-config
 
-// build-marker: v35-landscape-camera-fix-config
+// build-marker: v36-moon-landscape-zoom-config
 
-// build-marker: v35-landscape-camera-fix-config
+// build-marker: v36-moon-landscape-zoom-config
 
-// build-marker: v35-landscape-camera-fix-config
+// build-marker: v36-moon-landscape-zoom-config
+
+// build-marker: v36-moon-landscape-zoom-config
