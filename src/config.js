bc56c82f9fefa0d72bbd590a53b85e506e2b00fc
jimvankuik <jim.vankuik@gmail.com>
2026-05-19 23:26:05 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v54-double-jump",
+  version: "v55-double-jump-active",
   worldWidth: 4800,
   gravityY: 1680,
   playerSpeed: 335,
@@ -9,47 +9,49 @@ window.FTTM.GameSettings = {
   jumpCutVelocity: -310,
   acceleration: 2400,
   deceleration: 2850,
-  buildMarker: "v54-double-jump-config"
+  buildMarker: "v55-double-jump-active-config"
 };
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump
+// build-marker: v55-double-jump-active
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
 
-// build-marker: v54-double-jump-config
+// build-marker: v55-double-jump-active-config
+
+// build-marker: v55-double-jump-active-config
