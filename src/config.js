222d9dccf5468a62718d1b2afd1227589b5fd406
jimvankuik <jim.vankuik@gmail.com>
2026-05-19 23:12:39 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v52-camera-deadzone",
+  version: "v53-smooth-left-camera",
   worldWidth: 4800,
   gravityY: 1680,
   playerSpeed: 335,
@@ -9,43 +9,45 @@ window.FTTM.GameSettings = {
   jumpCutVelocity: -310,
   acceleration: 2400,
   deceleration: 2850,
-  buildMarker: "v52-camera-deadzone-config"
+  buildMarker: "v53-smooth-left-camera-config"
 };
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone
+// build-marker: v53-smooth-left-camera
 
-// build-marker: v52-camera-deadzone-config
+// build-marker: v53-smooth-left-camera-config
 
-// build-marker: v52-camera-deadzone-config
+// build-marker: v53-smooth-left-camera-config
 
-// build-marker: v52-camera-deadzone-config
+// build-marker: v53-smooth-left-camera-config
 
-// build-marker: v52-camera-deadzone-config
+// build-marker: v53-smooth-left-camera-config
 
-// build-marker: v52-camera-deadzone-config
+// build-marker: v53-smooth-left-camera-config
 
-// build-marker: v52-camera-deadzone-config
+// build-marker: v53-smooth-left-camera-config
 
-// build-marker: v52-camera-deadzone-config
+// build-marker: v53-smooth-left-camera-config
+
+// build-marker: v53-smooth-left-camera-config
