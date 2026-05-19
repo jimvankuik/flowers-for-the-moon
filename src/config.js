2c15306ab4c5dac1e43c4d2aead110e2d5964101
jimvankuik <jim.vankuik@gmail.com>
2026-05-19 23:20:14 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v53-smooth-left-camera",
+  version: "v54-double-jump",
   worldWidth: 4800,
   gravityY: 1680,
   playerSpeed: 335,
@@ -9,45 +9,47 @@ window.FTTM.GameSettings = {
   jumpCutVelocity: -310,
   acceleration: 2400,
   deceleration: 2850,
-  buildMarker: "v53-smooth-left-camera-config"
+  buildMarker: "v54-double-jump-config"
 };
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera
+// build-marker: v54-double-jump
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
 
-// build-marker: v53-smooth-left-camera-config
+// build-marker: v54-double-jump-config
+
+// build-marker: v54-double-jump-config
