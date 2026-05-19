5e6e9e67feb5e5c276b1ab4ebb0ffab69ba69591
jimvankuik <jim.vankuik@gmail.com>
2026-05-19 13:00:25 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,39 +1,39 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "v45-visible-finish-route",
-  worldWidth: 4300,
+  version: "v46-camera-finish-lock",
+  worldWidth: 4800,
   gravityY: 1680,
   playerSpeed: 335,
   jumpVelocity: -690,
   jumpCutVelocity: -310,
   acceleration: 2400,
   deceleration: 2850,
-  buildMarker: "v45-visible-finish-route-config"
+  buildMarker: "v46-camera-finish-lock-config"
 };
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route
+// build-marker: v46-camera-finish-lock
 
-// build-marker: v45-visible-finish-route-config
+// build-marker: v46-camera-finish-lock-config
