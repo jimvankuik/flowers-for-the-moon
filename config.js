486c4301683c60161d3fb0851be44dadb93f4dfd
jimvankuik <jim.vankuik@gmail.com>
2026-05-21 16:00:21 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+window.FTTM = window.FTTM || {};
+window.FTTM.GameSettings = {
+  version: "fluistervelden-prototype-v2.3-camera-ground-fix",
+  worldWidth: 6600,
+  gravityY: 1580,
+  playerSpeed: 325,
+  jumpVelocity: -680,
+  jumpCutVelocity: -315,
+  doubleJumpVelocity: -560,
+  acceleration: 2300,
+  deceleration: 2800,
+  buildMarker: "fluistervelden-prototype-v2.3-camera-ground-fix"
+};
