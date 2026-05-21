486f4c637b818dc48efdb9a44a55c7ab837255a0
jimvankuik <jim.vankuik@gmail.com>
2026-05-22 00:25:15 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v3.5-moon-boy-camera-smoothing",
-  worldWidth: 6400,
+  version: "fluistervelden-prototype-v4-intro-sequence-clean-rebuild",
+  worldWidth: 5200,
   gravityY: 1500,
   playerSpeed: 305,
   jumpVelocity: -660,
   jumpCutVelocity: -300,
   acceleration: 2300,
   deceleration: 2750,
-  buildMarker: "fluistervelden-prototype-v3.5-moon-boy-camera-smoothing-config"
+  buildMarker: "fluistervelden-prototype-v4-intro-sequence-clean-rebuild-config"
 };
