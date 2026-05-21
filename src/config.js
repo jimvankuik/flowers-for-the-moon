9d4d7c87718636e5efd291e3ae24a58ededca6da
jimvankuik <jim.vankuik@gmail.com>
2026-05-21 23:21:07 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v3.1-moon-fix",
-  worldWidth: 3900,
+  version: "fluistervelden-prototype-v3.2-camera-end-fix",
+  worldWidth: 5600,
   gravityY: 1500,
   playerSpeed: 305,
   jumpVelocity: -660,
   jumpCutVelocity: -300,
   acceleration: 2300,
   deceleration: 2750,
-  buildMarker: "fluistervelden-prototype-v3.1-moon-fix-config"
+  buildMarker: "fluistervelden-prototype-v3.2-camera-end-fix-config"
 };
