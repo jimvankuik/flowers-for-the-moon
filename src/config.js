0f972cec4609fc92bb568b5e67532aad186fc56a
jimvankuik <jim.vankuik@gmail.com>
2026-05-21 16:01:00 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 window.FTTM = window.FTTM || {};
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v2.2-scene-start-fix",
+  version: "fluistervelden-prototype-v2.3-camera-ground-fix",
   worldWidth: 6600,
   gravityY: 1580,
   playerSpeed: 325,
@@ -9,5 +9,5 @@ window.FTTM.GameSettings = {
   doubleJumpVelocity: -560,
   acceleration: 2300,
   deceleration: 2800,
-  buildMarker: "fluistervelden-prototype-v2.2-scene-start-fix"
+  buildMarker: "fluistervelden-prototype-v2.3-camera-ground-fix"
 };
