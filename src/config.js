365b1279f975ea114695fade9b3cd11c9ac64696
jimvankuik <jim.vankuik@gmail.com>
2026-05-21 15:45:50 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 window.FTTM = window.FTTM || {};
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v2",
+  version: "fluistervelden-prototype-v2.1-visible-world-fix",
   worldWidth: 6600,
   gravityY: 1580,
   playerSpeed: 325,
@@ -9,5 +9,5 @@ window.FTTM.GameSettings = {
   doubleJumpVelocity: -560,
   acceleration: 2300,
   deceleration: 2800,
-  buildMarker: "fluistervelden-prototype-v2"
+  buildMarker: "fluistervelden-prototype-v2.1-visible-world-fix"
 };
