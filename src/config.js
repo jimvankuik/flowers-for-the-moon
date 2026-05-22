be2c6880a732f1358bad42c8c491feedc48fe18e
jimvankuik <jim.vankuik@gmail.com>
2026-05-22 14:32:48 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v4-1-composition-pass",
+  version: "fluistervelden-prototype-v4-3-dynamic-camera",
   worldWidth: 5200,
   gravityY: 1500,
   playerSpeed: 305,
@@ -9,5 +9,5 @@ window.FTTM.GameSettings = {
   jumpCutVelocity: -300,
   acceleration: 2300,
   deceleration: 2750,
-  buildMarker: "fluistervelden-prototype-v4-1-composition-pass-config"
+  buildMarker: "fluistervelden-prototype-v4-3-dynamic-camera-config"
 };
