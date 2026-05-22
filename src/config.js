f3d387ea4ec8032b920e53aced4814edd9307edf
jimvankuik <jim.vankuik@gmail.com>
2026-05-22 15:25:07 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v4-4-real-dynamic-camera",
+  version: "fluistervelden-prototype-v4-5-visible-dynamic-camera",
   worldWidth: 5200,
   gravityY: 1500,
   playerSpeed: 305,
@@ -9,5 +9,5 @@ window.FTTM.GameSettings = {
   jumpCutVelocity: -300,
   acceleration: 2300,
   deceleration: 2750,
-  buildMarker: "fluistervelden-prototype-v4-4-real-dynamic-camera-config"
+  buildMarker: "fluistervelden-prototype-v4-5-visible-dynamic-camera-config"
 };
