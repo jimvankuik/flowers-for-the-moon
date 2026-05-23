1b86fecfc747648bdd1a39207216521472a57cfa
jimvankuik <jim.vankuik@gmail.com>
2026-05-23 09:56:23 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 window.FTTM = window.FTTM || {};
 window.FTTM.GameSettings = {
-  version: 'fluistervelden-prototype-v7.1-art-background-fix',
-  playerSpeed: 265,
+  version: 'fluistervelden-prototype-v7.2-embedded-art-fix',
+  playerSpeed: 245,
   acceleration: 1450,
   deceleration: 1750,
-  jumpVelocity: -610,
-  jumpCutVelocity: -240,
-  gravity: 1550
+  jumpVelocity: -590,
+  jumpCutVelocity: -235,
+  gravity: 1500
 };
