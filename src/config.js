f4e48efdb8bcc1db08a05d264976fbcf0c3d138e
jimvankuik <jim.vankuik@gmail.com>
2026-05-23 11:01:08 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v8.0-vertical-slice-foundation",
-  worldWidth: 3400,
+  version: "fluistervelden-prototype-v8.1-composition-zoom-fix",
+  worldWidth: 3200,
   gravityY: 1500,
   playerSpeed: 265,
   jumpVelocity: -610,
   jumpCutVelocity: -280,
   acceleration: 2200,
   deceleration: 2700,
-  buildMarker: "fluistervelden-prototype-v8.0-vertical-slice-foundation-config"
+  buildMarker: "fluistervelden-prototype-v8.1-composition-zoom-fix-config"
 };
