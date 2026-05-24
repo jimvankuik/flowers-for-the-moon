56abcb27862154d77a09b1e57310cb9849f90493
jimvankuik <jim.vankuik@gmail.com>
2026-05-24 23:42:52 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v8.2-assets-zoom-fix",
-  worldWidth: 3200,
+  version: "fluistervelden-prototype-v8.3-high-hill-double-jump",
+  worldWidth: 4200,
   gravityY: 1500,
   playerSpeed: 265,
   jumpVelocity: -610,
   jumpCutVelocity: -280,
   acceleration: 2200,
   deceleration: 2700,
-  buildMarker: "fluistervelden-prototype-v8.2-assets-zoom-fix-config"
+  buildMarker: "fluistervelden-prototype-v8.3-high-hill-double-jump-config"
 };
