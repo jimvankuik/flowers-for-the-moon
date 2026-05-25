4d88af7963cc31de290a3c155d5c1f772f582c5f
jimvankuik <jim.vankuik@gmail.com>
2026-05-25 20:59:29 +0200

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 window.FTTM = window.FTTM || {};
 
 window.FTTM.GameSettings = {
-  version: "fluistervelden-prototype-v8.5-stable-camera-assets",
+  version: "fluistervelden-prototype-v8.6-correct-assets-camera",
   worldWidth: 4200,
   gravityY: 1500,
-  playerSpeed: 265,
+  playerSpeed: 295,
   jumpVelocity: -610,
   jumpCutVelocity: -280,
-  acceleration: 2200,
-  deceleration: 2700,
-  buildMarker: "fluistervelden-prototype-v8.5-stable-camera-assets-config"
+  acceleration: 2850,
+  deceleration: 3300,
+  buildMarker: "fluistervelden-prototype-v8.6-correct-assets-camera-config"
 };
